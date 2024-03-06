cfaa1b1e3ba20c170dafc1aef9b1ee2c2ccd89ad
Diav3l <grandtheftcole@gmail.com>
2024-03-06 08:52:30 -0700

    edited doc strings to be more descriptive and fix errors

--- Encryption.py
+++ Encryption.py
@@ -7,7 +7,11 @@ class Encryption:
     """uses variable ROT encryption and alphabet rotation derived from sha3_512 hashing and stacking the users password """
 
     def __init__(self, hashedPassword):
-        """Generates a unique character list based on the given string"""
+        """Generates a unique character list used for encryption based on the given string
+
+        This means that the users password is the encryption and decryption key, therefore it is incredibly important
+        that it is safeguarded because a lost password is a lost account and no recovery options are possible.
+        """
         # Do not ever modify the below string, it will prevent proper decryption and all data will be lost
         alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`~!@#$%^&*()_-=|\}]{[\"':;?/>.<, "
         for i in range(500000):
@@ -19,21 +23,29 @@ class Encryption:
         self.verify_hash = hashedPassword
 
     def encrypt(self, string: str) -> str:
-        """:returns ciphertext of given plaintext"""
+        """ converts plaintext to ciphertext
+        :returns ciphertext: str
+        """
         rotation = secrets.randbelow(len(self.charSet))
         Salt = secrets.randbelow(100)
         encoded = "".join([self.charSet[(self.charSet.find(c) + rotation) % len(self.charSet)] for c in string])
         return encoded + str("{:04d}".format(rotation * Salt)) + str("{:02d}".format(Salt))
 
     def decrypt(self, string: str) -> str:
-        """:returns plaintext of given ciphertext"""
+        """Converts plaintext of given ciphertext
+        :returns plaintext: str
+        """
         rotation = int(string[-6:-2]) // int(string[-2:])
         actual = string[:-6]
         return "".join([self.charSet[(self.charSet.find(c) - rotation) % len(self.charSet)] for c in actual])
 
     @staticmethod
     def generate(length=30, special_characters=True) -> str:
-        """:returns a unique password of chosen length"""
+        """Generates a unique password using pythons secrets librairy
+        :param length: int, defaults to 30
+        :param special_characters: bool, defaults to True
+        :return password: str, of requested length and character usage
+        """
         if special_characters:
             return str(secrets.token_bytes(length))[1:length+1]
         else:
@@ -41,14 +53,18 @@ class Encryption:
 
     @staticmethod
     def stack(hashed_string: hashlib.sha3_512) -> hashlib.sha3_512:
-        """:returns hash_string stacked 1 million times"""
+        """Stacks the given string in a sha3_512 hash 1 million times
+        :return hashed_string: _Hash
+        """
         for i in range(1000000):
             hashed_string = hashlib.sha3_512(hashed_string.digest() + hashed_string.digest())
         return hashed_string
 
     @staticmethod
-    def generate_noise(number_of_files: int):
-        """Creates files of random length with noise that looks the same as actual user data"""
+    def generate_noise(number_of_files: int) -> None:
+        """Creates files of random length with noise that looks the same as actual user data
+        :param number_of_files: int
+        """
         import FileHandler
         alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`~!@#$%^&*()_-=|\}]{[\"':;?/>.<, "
         i = len(alphabet)
